135aa2d9201e8640976151e74a7ad0d6c2c49487
Moritz Duge <moritz.duge@allotropia.de>
2025-02-26 22:29:42 +0100

    Revert "WIP: desktop LOWA like build with impress enabled"
    This reverts commit 5e1009c15a7c99e36096e485e5635cef81730147.

--- RepositoryExternal.mk
+++ RepositoryExternal.mk
@@ -1797,41 +1797,41 @@ endif
 
 endif # SYSTEM_ODFGEN
 
-#ifneq ($(SYSTEM_EPUBGEN),)
-#
-#define gb_LinkTarget__use_epubgen
-#$(call gb_LinkTarget_set_include,$(1),\
-#	$$(INCLUDE) \
-#    $(EPUBGEN_CFLAGS) \
-#)
-#$(call gb_LinkTarget_add_libs,$(1),$(EPUBGEN_LIBS))
-#
-#endef
-#gb_ExternalProject__use_epubgen :=
-#
-#else # !SYSTEM_EPUBGEN
-#####ifneq ($(ENABLE_WASM_STRIP_EPUB),TRUE)
-#####ifneq ($(ENABLE_WASM_STRIP_EPUB),TRUE)
-#
-#define gb_LinkTarget__use_epubgen
-#$(call gb_LinkTarget_set_include,$(1),\
-#	-I$(gb_UnpackedTarball_workdir)/libepubgen/inc \
-#	$$(INCLUDE) \
-#)
-#$(call gb_LinkTarget_add_libs,$(1),\
-#	$(gb_UnpackedTarball_workdir)/libepubgen/src/lib/.libs/libepubgen-0.1$(gb_StaticLibrary_PLAINEXT) \
-#)
-#$(call gb_LinkTarget_use_external_project,$(1),libepubgen)
-#
-#endef
-#define gb_ExternalProject__use_epubgen
-#$(call gb_ExternalProject_use_external_project,$(1),libepubgen)
-#
-#endef
-#
-#####endif # ENABLE_WASM_STRIP_EPUB
-#####endif # ENABLE_WASM_STRIP_EPUB
-#endif # SYSTEM_EPUBGEN
+ifneq ($(SYSTEM_EPUBGEN),)
+
+define gb_LinkTarget__use_epubgen
+$(call gb_LinkTarget_set_include,$(1),\
+	$$(INCLUDE) \
+    $(EPUBGEN_CFLAGS) \
+)
+$(call gb_LinkTarget_add_libs,$(1),$(EPUBGEN_LIBS))
+
+endef
+gb_ExternalProject__use_epubgen :=
+
+else # !SYSTEM_EPUBGEN
+####ifneq ($(ENABLE_WASM_STRIP_EPUB),TRUE)
+ifneq ($(ENABLE_WASM_STRIP_EPUB),TRUE)
+
+define gb_LinkTarget__use_epubgen
+$(call gb_LinkTarget_set_include,$(1),\
+	-I$(gb_UnpackedTarball_workdir)/libepubgen/inc \
+	$$(INCLUDE) \
+)
+$(call gb_LinkTarget_add_libs,$(1),\
+	$(gb_UnpackedTarball_workdir)/libepubgen/src/lib/.libs/libepubgen-0.1$(gb_StaticLibrary_PLAINEXT) \
+)
+$(call gb_LinkTarget_use_external_project,$(1),libepubgen)
+
+endef
+define gb_ExternalProject__use_epubgen
+$(call gb_ExternalProject_use_external_project,$(1),libepubgen)
+
+endef
+
+####endif # ENABLE_WASM_STRIP_EPUB
+endif # ENABLE_WASM_STRIP_EPUB
+endif # SYSTEM_EPUBGEN
 
 ifneq ($(SYSTEM_REVENGE),)
 
